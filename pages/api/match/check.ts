80a3f1b3c1a321e7909d7ef63df98044d52be7a2
Makar <makar@fuek.ru>
2025-06-20 13:45:59 +0300

    fix: remove invalid character from .or() queries

--- pages/api/match/check.ts
+++ pages/api/match/check.ts
@@ -7,8 +7,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const { data } = await supabase
     .from('likes')
     .select('*')
-    .or(\`from.eq.\${user1},from.eq.\${user2}\`)
-    .or(\`to.eq.\${user1},to.eq.\${user2}\`);
+    .or(`from.eq.${user1},from.eq.${user2}`)
+    .or(`to.eq.${user1},to.eq.${user2}`);
 
   const likedEachOther = data?.filter(l => l.from === user1 && l.to === user2).length &&
                          data?.filter(l => l.from === user2 && l.to === user1).length;
