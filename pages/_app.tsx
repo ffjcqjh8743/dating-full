d782d1190b5f53dae23d5232c58e57bffa791ddc
Makar <makar@fuek.ru>
2025-06-20 15:31:39 +0300

    fix: correct import path for BottomNav

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import BottomNav from '@/components/BottomNav';
+import BottomNav from '../components/BottomNav';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
