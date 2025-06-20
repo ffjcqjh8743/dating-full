bf22542e0bf11269f1c290a9bc0b97fe98509369
Makar <makar@fuek.ru>
2025-06-20 13:41:30 +0300

    fix: valid JSX for TelegramLink

--- components/TelegramLink.tsx
+++ components/TelegramLink.tsx
@@ -7,7 +7,7 @@ export default function TelegramLink({ username, isMatched }: Props) {
   return (
     <div className="mt-2 text-center">
       {isMatched ? (
-        <a href={\`https://t.me/\${username}\`} className="text-blue-500 underline">Открыть Telegram профиль</a>
+        <a href={`https://t.me/${username}`} className="text-blue-500 underline">Открыть Telegram профиль</a>
       ) : (
         <span className="text-gray-500 text-sm">🔒 Профиль Telegram доступен только при взаимном лайке</span>
       )}
